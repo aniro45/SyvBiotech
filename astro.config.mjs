cc2c70a7cf461ef041c582865ed60a5261c0679b
Aniket Jadhav <ajstyle45@Anikets-MacBook.local>
2025-05-29 23:18:14 +0530

    added search engine optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    base: './',
-})
+  output: "static",
+  base: "./",
+  site: "https://syvbiotech.com", // Replace with your actual domain
+  integrations: [sitemap()],
+});
